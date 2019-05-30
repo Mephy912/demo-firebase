981ab7af2c570394749ceec397e461b7f98aaf30
Mephy912 <qwaszx8411@gmail.com>
2019-05-30 14:33:05 +0800

    Add express body-parser

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +8,18 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+export const webApi = functions.https.onRequest(main);
+
+app.get('/demo',(req, res) => {
+    res.status(200).send({
+      result: [1,2,3,4,5,6,7]
+    });
+});
